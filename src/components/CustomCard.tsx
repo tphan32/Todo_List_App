9c1fbbc9edd03702aacc0fa8d6ee3d51ac6ad1c6
Tinh Phan <pttinh0804@gmail.com>
2024-02-25 16:06:49 +0700

    feat: add update status icon and remove icon for cards

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -9,6 +9,11 @@ import TodoImg from "../assets/todo.jpg";
 import Chip from "@mui/material/Chip";
 import { Status, Task } from "../pages/Dashboard";
 import { useNavigate } from "react-router-dom";
+import DeleteIcon from "@mui/icons-material/Delete";
+import DoneIcon from "@mui/icons-material/Done";
+import IconButton from "@mui/material/IconButton/IconButton";
+import ClearIcon from "@mui/icons-material/Clear";
+import { useAppContext } from "./store/AppContext";
 
 type CustomCardProps = Task;
 
@@ -19,23 +24,35 @@ export default function CustomCard({
   status,
 }: CustomCardProps) {
   const navigate = useNavigate();
+  const { removeTask, updateTaskStatus } = useAppContext();
+  const isTaskCompleted = status === Status.COMPLETED;
+
+  const handleComplete = (id: string) => {
+    if (isTaskCompleted) {
+      updateTaskStatus(id, Status.PENDING);
+    } else {
+      updateTaskStatus(id, Status.COMPLETED);
+    }
+  };
 
   return (
     <Card className="flex flex-col">
       <CardMedia component="div" image={TodoImg} className="pt-40" />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2" noWrap>
+      <CardContent className="flex flex-col gap-y-3">
+        <Typography variant="h5" component="h2" noWrap>
           {title}
         </Typography>
         <Typography variant="body2" noWrap>
           {description}
+        </Typography>
+        <Typography variant="subtitle2" noWrap>
+          Status:
         </Typography>
         <Chip
           label={status.toUpperCase()}
           color={`${
             status.toUpperCase() === Status.COMPLETED ? "success" : "warning"
           }`}
-          className="mt-4"
           size="medium"
         />
       </CardContent>
@@ -56,6 +73,17 @@ export default function CustomCard({
         >
           Edit
         </Button>
+        <IconButton
+          aria-label="complete"
+          onClick={() => {
+            handleComplete(id);
+          }}
+        >
+          {isTaskCompleted ? <ClearIcon /> : <DoneIcon />}
+        </IconButton>
+        <IconButton aria-label="delete" onClick={() => removeTask(id)}>
+          <DeleteIcon />
+        </IconButton>
       </CardActions>
     </Card>
   );
